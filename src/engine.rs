ba667706478d23fe0d6153a8ada4e61bca7ad2ce
Dawid Komęza <dawid.komeza@gmail.com>
2023-02-11 15:26:07 +0000

    Changed engine creation method

--- src/engine.rs
+++ src/engine.rs
@@ -3,3 +3,15 @@ pub struct Engine {
     pub _author: &'static str,
     pub _version: &'static str,
 }
+
+impl Engine {
+    pub fn new(name: &'static str, author: &'static str, version: &'static str) -> Engine {
+        Engine {
+            _name: name,
+            _author: author,
+            _version: version,
+        }
+    }
+}
+
+// Path: src/uci.rs
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@ pub mod engine;
 pub mod uci;
 
 fn main() {
-    let engine = engine::Engine {
-        _name: "Rusty",
-        _author: "Rusty",
-        _version: "0.1",
-    };
+    let engine = engine::Engine::new("RustChess", "RustChess", "0.1");
     let mut uci = uci::Uci { engine };
     loop {
         let mut command = String::new();
